c668865cd31118f7637dae743550f7e07124783e
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2025-04-13 14:58:46 -0300

    feat: add Time class for sports team management

---
+++ 1_Criação_Classes_PT1/Q1/Teste.java
@@ -0,0 +1,14 @@
+public class Teste {
+    public static void main(String[] args) {
+        Time t1 = new Time("Flamengo");
+        
+        t1.registrarVitoria();
+        t1.registrarVitoria();
+        t1.registrarVitoria();
+        t1.registrarEmpate();
+        t1.registrarDerrota();
+        
+        System.out.println(t1);
+        System.out.printf("Aproveitamento: %.2f%%\n", t1.calcAproveitamento());
+    }
+}
---
+++ 1_Criação_Classes_PT1/Q1/Time.java
@@ -0,0 +1,41 @@
+public class Time {
+    private String nome;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+    private int pontos;
+
+    public Time(String nome) {
+        this.nome = nome;
+        this.vitorias = 0;
+        this.derrotas = 0;
+        this.empates = 0;
+        this.pontos = 0;
+    }
+
+    public void registrarVitoria() {
+        vitorias++;
+        pontos += 3;
+    }
+
+    public void registrarDerrota() {
+        derrotas++;
+    }
+
+    public void registrarEmpate() {
+        empates++;
+        pontos += 1;
+    }
+
+    public double calcularAproveitamento() {
+        int totalJogos = vitorias + derrotas + empates;
+        if (totalJogos == 0) return 0;
+        return (pontos * 100.0) / (totalJogos * 3);
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - Pontos: " + pontos + " (V: " + vitorias + 
+               ", D: " + derrotas + ", E: " + empates + ")";
+    }
+}
