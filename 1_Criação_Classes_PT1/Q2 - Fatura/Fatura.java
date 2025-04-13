34a4d2a8522d87e18dc18c3bb4b62f773e10deed
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2025-04-13 15:43:30 -0300

    feat: add Fatura class

---
+++ 1_Criação_Classes_PT1/Q2 - Fatura/Fatura.java
@@ -0,0 +1,26 @@
+public class Fatura{
+    private String id;
+    private String descricao;
+    private int quantidade;
+    private double preco_unitario;
+    
+    public Fatura(String id, String descricao, int quantidade, double preco_unitario){
+        this.id = id;
+        this.descricao = descricao;
+        this.quantidade = quantidade < 0 ? 0 : quantidade;
+        this.preco_unitario = preco_unitario < 0 ? 0.0 : preco_unitario;
+    }
+    
+    public double calculaTotal(){
+        return quantidade * preco_unitario;
+    }
+    
+    @Override
+    public String toString(){
+        return "Fatura - id: " + id + 
+        "\nDescrição : " + descricao + 
+        "\nQuantidade: " + quantidade + 
+        "\nPreço por unidade: " + preco_unitario +
+        "\nValor Total: " + calculaTotal();
+    }
+}
---
+++ 1_Criação_Classes_PT1/Q2 - Fatura/FaturaTeste.java
@@ -0,0 +1,9 @@
+public class FaturaTeste{
+    public static void main(String[] args){
+        Fatura f1 = new Fatura("001", "Celular", 2, 2500.50);
+        Fatura f2 = new Fatura("002", "Mouse", 1, 150.00);
+        
+        System.out.println(f1);
+        System.out.println("\n" + f2);
+    }
+}
