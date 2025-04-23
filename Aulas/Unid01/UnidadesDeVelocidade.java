f90b9e09d3c0a2b307a3949f1c09618e7c1ff49b
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2025-04-23 18:51:43 -0300

    add UnidadesDeVelocidade class

---
+++ Aulas/Unid01/UnidadesDeVelocidade.java
@@ -0,0 +1,13 @@
+public class UnidadesDeVelocidade {
+    public static void main(String[] args){
+        double kph = 0.0, mps;
+
+        System.out.println("k/h \t m/s");
+
+        while (kph <= 50.0){
+            mps = kph * 0.2778;
+            System.out.println(kph + "\t" + kps);
+            kph += 0.5;
+        }
+    }
+}
