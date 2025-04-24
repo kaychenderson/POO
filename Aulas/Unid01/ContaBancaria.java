be754eebec149dcec700686a1c8eed4f8fe24b0d
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2025-04-24 20:25:16 -0300

    Update ContaBancaria class

--- Aulas/Unid01/ContaBancaria.java
+++ Aulas/Unid01/ContaBancaria.java
@@ -1,40 +1,50 @@
 public class ContaBancaria {
-    private String nome;
     private String cpf;
+    private String nome;
     private double saldo;
 
-    public ContaBancaria (String nome, String cpf, double saldo) {
-        this.nome = nome;
+    // Construtor
+    public ContaBancaria(String cpf, String nome, double saldo) {
         this.cpf = cpf;
+        this.nome = nome;
         this.saldo = saldo;
     }
 
-    public void depositar (double valor) {
+    // Método para depositar
+    public void depositar(double valor) {
         if (valor > 0) {
-            saldo = saldo + valor;
-            System.out.println("Deposito de R$" + valor + " realizado com sucesso.");
+            saldo += valor;
+            System.out.println("Depósito de R$" + valor + " realizado com sucesso.");
         } else {
-            System.out.println("Valor informado inválido.");
+            System.out.println("Valor de depósito inválido.");
         }
 
-        System.out.println("Saldo final: R$" + saldo + ".");
+        // System.out.println("Saldo atual de " + nome + " (" + cpf + "): R$" + saldo);
     }
 
-    public void sacar (double valor) {
-        if (valor > 0 && valor < saldo) {
-            saldo = saldo - valor;
+    // Método para sacar
+    public void sacar(double valor) {
+        if (valor > 0 && valor <= saldo) {
+            saldo -= valor;
             System.out.println("Saque de R$" + valor + " realizado com sucesso.");
         } else {
-            System.out.println("Valor informado inválido.");
+            System.out.println("Saldo insuficiente ou valor inválido.");
         }
 
-        System.out.println("Saldo final: R$" + saldo + ".");
+        // System.out.println("Saldo atual de " + nome + " (" + cpf + "): R$" + saldo);
+    }
+
+    // Método para exibir o saldo
+    public void exibirSaldo() {
+        System.out.println("Saldo atual de " + nome + " (" + cpf + "): R$" + saldo);
     }
 
     public static void main(String[] args) {
-        ContaBancaria c1 = new ContaBancaria("João Lucas", "123.456.789", 1000);
-
-        c1.depositar(200);
-        c1.sacar(400);
+        ContaBancaria c1 = new ContaBancaria("123.456.789-00", "João Silva", 1000.0);
+        c1.exibirSaldo();
+        c1.depositar(200.0);
+        c1.exibirSaldo();
+        c1.sacar(500.0);
+        c1.exibirSaldo();
     }
 }
