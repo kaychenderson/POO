6ca9d60a8b6f86c2d80b31df016fe0ed35500b83
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2025-05-02 20:02:44 -0300

    add ConversorMinutos class

---
+++ 0_Intro_ao_Java/06_ConversorMinutos/ConversorMinutos.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ConversorMinutos {
+    public static void main(String[] args) {
+
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.print("Digite o valor em minutos: ");
+        int minutosTotais = scanner.nextInt();
+
+        int dias = minutosTotais / (24 * 60); // 1 dia = 1440 minutos
+        int horas = (minutosTotais % (24 * 60)) / 60;
+        int minutos = minutosTotais % 60;
+
+        System.out.printf("%d minutos = %d dias, %d horas e %d minutos%n", minutosTotais, dias, horas, minutos);
+        
+        leitor.close();
+    }
+}
