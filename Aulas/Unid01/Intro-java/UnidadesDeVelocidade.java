c45cbbc18a1f4ba473f3f3357fe1ab66db55b7a4
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2025-05-01 22:44:51 -0300

    Update UnidadesDeVelocidade class

--- Aulas/Unid01/Intro-java/UnidadesDeVelocidade.java
+++ Aulas/Unid01/Intro-java/UnidadesDeVelocidade.java
@@ -1,13 +1,41 @@
+import java.text.DecimalFormat;
+
 public class UnidadesDeVelocidade {
-    public static void main(String[] args){
-        double kph = 0.0, mps;
+    public static void main(String[] args) {
+        double kph = 0.0, mps, mph, pps;
 
-        System.out.println("k/h \t m/s");
+        DecimalFormat df = new DecimalFormat("00.00");
 
-        while (kph <= 50.0){
+        System.out.println("k/h \t m/s \t m/h \t ml/s");
+
+        while (kph <= 50.0) {
             mps = kph * 0.2778;
-            System.out.println(kph + "\t" + mps);
+            mph = kph * 0.6214;
+            pps = kph * 0.9113;
+
+            System.out.println(
+                df.format(kph) + "\t" +
+                df.format(mps) + "\t" +
+                df.format(mph) + "\t" +
+                df.format(pps)
+            );
+
             kph += 0.5;
         }
     }
 }
+
+
+// public class UnidadesDeVelocidade {
+//     public static void main(String[] args){
+//         double kph = 0.0, mps;
+
+//         System.out.println("k/h \t m/s");
+
+//         while (kph <= 50.0){
+//             mps = kph * 0.2778;
+//             System.out.println(kph + "\t" + mps);
+//             kph += 0.5;
+//         }
+//     }
+// }
