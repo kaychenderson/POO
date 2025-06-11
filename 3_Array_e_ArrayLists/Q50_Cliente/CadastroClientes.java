ff8833c7054577d103c4848adc7f08172316620b
Kayc Henderson <146909378+kaychenderson@users.noreply.github.com>
2025-06-11 16:55:56 -0300

    fixing code

--- 3_Array_e_ArrayLists/Q50_Cliente/CadastroClientes.java
+++ 3_Array_e_ArrayLists/Q50_Cliente/CadastroClientes.java
@@ -3,6 +3,9 @@ package 3_Array_e_ArrayLists.Q50_Cliente;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CadastroClientes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -11,13 +14,15 @@ public class CadastroClientes {
         while (true) {
             System.out.print("Digite o ID do cliente (negativo para sair): ");
             int id = sc.nextInt();
-            if (id < 0) break; 
+            if (id < 0) break;
+            sc.nextLine();
 
             System.out.print("Nome: ");
             String nome = sc.nextLine();
 
             System.out.print("Idade: ");
             int idade = sc.nextInt();
+            sc.nextLine();
 
             System.out.print("Telefone: ");
             String telefone = sc.nextLine();
